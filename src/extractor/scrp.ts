fe97f51ff1b3a29aacc21c75ed6ea4f772bf3a1e
Dusan Knezevic <dusan.knezevic@recordedfuture.com>
2025-04-21 16:43:32 +0200

    Property listing adata succesfully extracted and saved to data folder2

--- src/extractor/scrp.ts
+++ src/extractor/scrp.ts
@@ -24,16 +24,16 @@ export async function scrp(url: string): Promise<string | null> {
       console.error("❌ Failed to remove 'screens' property from apiData");
     }
 
-    // Save the apiData to src/data/api-data.json
+    // Save the apiData JSON to data folder
     await saveToFile(JSON.stringify(apiData), LISTING_DATA_PATHS.apiData);
 
-    // Transform the apiData to listing data and save to src/data/listing-data.json
+    // Transform the apiData to listing data and save listingData JSON to data folder
     const listingData = getListingData(apiData);
     if (listingData) {
       console.log("✔ Successfully transformed property listing data from apiData JSON to listingData JSON");
 
-      // Save the listing data to src/data/listing-data.json
-      console.log("➜ Saving listing data to listing-data.json");
+      // Save the listing data to data folder
+      console.log("➜ Saving listing data JSON to data folder");
       await saveToFile(JSON.stringify(listingData), LISTING_DATA_PATHS.listingData);
     } else {
       console.error("❌ Failed to transform property listing data from apiData JSON to listingData JSON");
